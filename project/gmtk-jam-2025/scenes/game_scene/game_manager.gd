1ebcfed403c18f48b62004703cb708f53413019e
DastardlySky <117837801+DastardlySky@users.noreply.github.com>
2025-07-31 18:25:53 +0100

    added stuff ;)

--- project/gmtk-jam-2025/scenes/game_scene/game_manager.gd
+++ project/gmtk-jam-2025/scenes/game_scene/game_manager.gd
@@ -10,6 +10,8 @@ var notePressedTime : float = 0
 
 @onready var animation_player: AnimationPlayer = %AnimationManager
 
+@export var staticShader: ColorRect
+
 @export var lifeTime : float = 2
 @export var rewindSpeed : float = -20
 @export var forwardSpeed : float = 2
@@ -33,18 +35,25 @@ func _unhandled_input(event: InputEvent) -> void:
 	# rewind the timeline
 	if Input.is_action_just_pressed("rewind"):
 		isRewinding = true
+		currentNoteDuration = 0.0 
 		animation_player.play("game",-1,rewindSpeed,true)
+		
 	elif Input.is_action_just_released("rewind"):
 		isRewinding = false
 		animation_player.play("game")
 		
 	# fast-forward 
-	if Input.is_action_just_pressed("forward"):
+	elif Input.is_action_just_pressed("forward"):
 		isForwarding = true
 		animation_player.play("game",-1,forwardSpeed,false)
 	elif Input.is_action_just_released("forward"):
 		isForwarding = false
 		animation_player.play("game")
+		
+	if isRewinding == true:
+		staticShader.visible = true
+	else:
+		staticShader.visible = false
 
 # the function used to place swear notes
 func swear_note_single() -> void:

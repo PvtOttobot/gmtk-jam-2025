0e865b540bdb772b168f5c1d31e1dd1d23a227ce
DastardlySky <117837801+DastardlySky@users.noreply.github.com>
2025-07-31 14:03:41 +0000

    Deploying to gh-pages from @ PvtOttobot/gmtk-jam-2025@e05d891e3a74908ffe305aaf1001a864f9e379d5 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1753969991|5256265';
+const CACHE_VERSION = '1753970608|8609292';
 /** @type {string} */
 const CACHE_PREFIX = 'Working Title-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;

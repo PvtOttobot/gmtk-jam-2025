69d0e57c7c10843a3c277e8f4d8d3afc9c169c86
Otto <danielottojackpetersen2@gmail.com>
2025-07-31 18:42:00 +0100

    panel ui

--- project/gmtk-jam-2025/scenes/ui/bottom_panel_button.gd
+++ project/gmtk-jam-2025/scenes/ui/bottom_panel_button.gd
@@ -1,18 +1,29 @@
 @tool
 extends Control
 
-var _text: String = "Default button text"
+var _texture_normal: AtlasTexture
+var _texture_pressed: AtlasTexture
 
-@onready var button: Button = $Button
+@onready var texture_button: TextureButton = $MarginContainer/TextureButton
 
-@export var text: String:
+@export var texture_normal: AtlasTexture:
 	get:
-		return _text
+		return _texture_normal
 	set(value):
-		_text = value
-		if is_node_ready() and is_instance_valid(button):
-			button.text = _text
-	
+		_texture_normal = value
+		apply_textures()
+
+@export var texture_pressed: AtlasTexture:
+	get:
+		return _texture_pressed
+	set(value):
+		_texture_pressed = value
+		apply_textures()
+
 func _ready() -> void:
-	if is_instance_valid(button):
-		button.text = _text
+	apply_textures()
+
+func apply_textures() -> void:
+	if is_node_ready() and is_instance_valid(texture_button):
+			texture_button.texture_normal = _texture_normal
+			texture_button.texture_pressed = _texture_pressed

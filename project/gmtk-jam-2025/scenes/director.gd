e549d56a5c9f44a693f0b4d8540972726201eb8e
acul4321 <lucarogers666@gmail.com>
2025-07-31 17:34:27 +0100

    main tscn layout

--- project/gmtk-jam-2025/scenes/director.gd
+++ project/gmtk-jam-2025/scenes/director.gd
@@ -2,6 +2,8 @@ extends Node
 
 @onready var progressBar: ProgressBar = $ProgressBar
 
+@export var GameManager : Node
+
 @export var goodReward : float = 5
 @export var badPunishment : float = -5
 @export var veryBadPunishment : float = -25
@@ -14,17 +16,16 @@ extends Node
 signal pullPlug
 
 func _process(delta: float) -> void:
-	
 	# if we're censoring and the host is swearing - IS GOOD! 
-	if (%GameManager.isCensoring == true) && (%GameManager.isSwearing == true):
+	if (GameManager.isCensoring == true) && (GameManager.isSwearing == true):
 		progressBar.value += goodReward * delta
 		
 	# if we're censoring and the host is not swearing - IS BAD! 
-	if (%GameManager.isCensoring == true) && (%GameManager.isSwearing == false):
+	if (GameManager.isCensoring == true) && (GameManager.isSwearing == false):
 		progressBar.value += badPunishment * delta
 		
 	# if we're not censoring and the host is swearing - IS VERY BAD! 
-	if (%GameManager.isCensoring == true) && (%GameManager.isSwearing == false):
+	if (GameManager.isCensoring == true) && (GameManager.isSwearing == false):
 		progressBar.value += veryBadPunishment * delta
 		
 	if (progressBar.value == 0):

495bd531302577e51d2c808059478405f4c6248d
PvtOttobot <danielottojackpetersen2@gmail.com>
2025-08-01 16:20:38 +0000

    Deploying to gh-pages from @ PvtOttobot/gmtk-jam-2025@fd34dd24420342094d254eec0abecbd2ac912771 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1754064506|14566267';
+const CACHE_VERSION = '1754065223|13521280';
 /** @type {string} */
 const CACHE_PREFIX = 'Working Title-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;

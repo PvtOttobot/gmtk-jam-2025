8283580a458a302b36d085a0a8efa952c86d48fb
Otto <danielottojackpetersen2@gmail.com>
2025-08-01 23:04:39 +0100

    commit

--- project/gmtk-jam-2025/scenes/game_scene/levels/main.gd
+++ project/gmtk-jam-2025/scenes/game_scene/levels/main.gd
@@ -2,10 +2,10 @@ extends Control
 
 signal level_won
 
-@onready var GameManager: Node = $HBoxContainer/Level1/GameManager
-
-func _ready() -> void:
-	GameManager.showEnd.connect(showEnd)
+#@onready var GameManager: Node = %GameManager
+#
+#func _ready() -> void:
+	#GameManager.showEnd.connect(showEnd)
 
 func showEnd():
 	level_won.emit()

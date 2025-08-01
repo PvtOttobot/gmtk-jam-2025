4a152f44eb55c2ff9cf9a955a95feba13c7a27ee
Acul4321 <124876210+Acul4321@users.noreply.github.com>
2025-08-01 17:36:13 +0000

    Deploying to gh-pages from @ PvtOttobot/gmtk-jam-2025@c8805bb897654ddc5b9fd0c43fe4545808feeb9e 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1754069315|13625990';
+const CACHE_VERSION = '1754069760|13526163';
 /** @type {string} */
 const CACHE_PREFIX = 'Working Title-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;

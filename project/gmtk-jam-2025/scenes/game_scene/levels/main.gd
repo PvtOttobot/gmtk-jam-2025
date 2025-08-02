923a7a9cc96422d82773766f23a97727620fff99
acul4321 <lucarogers666@gmail.com>
2025-08-02 15:17:02 +0100

    director sounds and logic

--- project/gmtk-jam-2025/scenes/game_scene/levels/main.gd
+++ project/gmtk-jam-2025/scenes/game_scene/levels/main.gd
@@ -2,10 +2,10 @@ extends Control
 
 signal level_won
 
-#@onready var GameManager: Node = %GameManager
-#
-#func _ready() -> void:
-	#GameManager.showEnd.connect(showEnd)
+@export var GameManager: Node
+
+func _ready() -> void:
+	GameManager.showEnd.connect(showEnd)
 
 func showEnd():
 	level_won.emit()

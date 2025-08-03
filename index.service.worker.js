0a57dc1de31da28f30df7319642c6e7450c77d63
DastardlySky <117837801+DastardlySky@users.noreply.github.com>
2025-08-03 03:28:37 +0000

    Deploying to gh-pages from @ PvtOttobot/gmtk-jam-2025@224a24ca516d0deb38f2a3e90361e1376d1ac467 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1754188750|40239201';
+const CACHE_VERSION = '1754191694|40144009';
 /** @type {string} */
 const CACHE_PREFIX = 'BLEEP-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;

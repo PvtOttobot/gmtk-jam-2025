dd6022e26d8afb71a096b19118bb40fdeb65eb89
Acul4321 <124876210+Acul4321@users.noreply.github.com>
2025-08-03 05:27:52 +0000

    Deploying to gh-pages from @ PvtOttobot/gmtk-jam-2025@e71912f091f2979b10002271725267d5f7d094f3 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1754197470|40340872';
+const CACHE_VERSION = '1754198851|40148909';
 /** @type {string} */
 const CACHE_PREFIX = 'BLEEP-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;

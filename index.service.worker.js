40c849d7518bfe9fdc78c6d371fce225d12e6111
PvtOttobot <PvtOttobot@users.noreply.github.com>
2025-08-03 16:31:14 +0000

    Deploying to gh-pages from @ PvtOttobot/gmtk-jam-2025@5f3a34cca625e3002d8cb4db3e5635d758a0b19a 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1754226995|41057635';
+const CACHE_VERSION = '1754238653|41184788';
 /** @type {string} */
 const CACHE_PREFIX = 'BLEEP-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
